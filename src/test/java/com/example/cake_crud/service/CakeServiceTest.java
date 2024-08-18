6bc19b50758bef7c54e29a5e9e35db72fa8621b9
Alissa Reynolds <144733508+alissareynolds@users.noreply.github.com>
2024-08-17 18:05:09 -0700

    added unit tests to service methods

--- src/test/java/com/example/cake_crud/service/CakeServiceTest.java
+++ src/test/java/com/example/cake_crud/service/CakeServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.cake_crud.service;
 
+import com.example.cake_crud.exception.CakeNotFoundException;
 import com.example.cake_crud.model.Cake;
 import com.example.cake_crud.repository.CakeRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CakeServiceTest {
 
-    private CakeService mockCakeService;
+    private CakeService cakeService;
     private CakeRepository mockCakeRepository;
 
     public final Cake input = new Cake(null, "German Chocolate", "chocolate", "brown", 3, true, false, false);
@@ -23,10 +24,72 @@ class CakeServiceTest {
     public final Cake recordWithId = new Cake(UUID.randomUUID(), "German Chocolate", "chocolate", "brown", 3, true, false, false);
     public final Cake recordWithId2 = new Cake(recordWithId.getId(), "Chantilly", "cream", "white", 4, true, true, false);
 
+    public final UUID id = UUID.fromString("59c47568-fde0-4dd7-9aef-03db6a962810");
+
     @BeforeEach
     public void setup() {
         mockCakeRepository = Mockito.mock(CakeRepository.class);
-        mockCakeService = new CakeService(mockCakeRepository);
+        cakeService = new CakeService(mockCakeRepository);
     }
 
+    @Test
+    public void create_shouldReturnCreatedCake() {
+        Mockito.when(mockCakeRepository.save(Mockito.any())).thenReturn(recordWithId);
+        Cake response = cakeService.create(input);
+        assertEquals(recordWithId, response);
+    }
+
+    @Test
+    public void getAll_shouldReturnListOfCakes() {
+        List<Cake> cakes = new ArrayList<>();
+        cakes.add(input);
+        cakes.add(input2);
+        Mockito.when(mockCakeRepository.findAll()).thenReturn(cakes);
+        List<Cake> response = cakeService.getAll();
+        assertEquals(cakes, response);
+    }
+
+    @Test
+    public void getById_shouldReturnCake() {
+        Mockito.when(mockCakeRepository.findById(recordWithId.getId())).thenReturn(Optional.of(recordWithId));
+        Cake response = cakeService.getById(recordWithId.getId());
+        assertEquals(recordWithId, response);
+    }
+
+    @Test
+    public void getById_throwsExceptionWhenBookWasNotFound() {
+        Mockito.when(mockCakeRepository.findById(id)).thenReturn(Optional.empty());
+        CakeNotFoundException exception = assertThrows(CakeNotFoundException.class, () -> cakeService.getById(id));
+        assertEquals("A cake with id: " + id + " was not found.", exception.getMessage());
+    }
+
+    @Test
+    public void getByType_shouldReturnListOfCakes() {
+        Mockito.when(mockCakeRepository.findByType(recordWithId.getType())).thenReturn(List.of(recordWithId));
+        List<Cake> response = cakeService.getByType(recordWithId.getType());
+        assertEquals(List.of(recordWithId), response);
+    }
+
+    @Test
+    public void update_shouldReturnUpdatedCake() {
+        Mockito.when(mockCakeRepository.findById(recordWithId.getId())).thenReturn(Optional.of(recordWithId));
+        Mockito.when(mockCakeRepository.save(Mockito.any())).thenReturn(recordWithId);
+        Cake response = cakeService.update(input2, recordWithId.getId());
+        assertEquals(recordWithId, response);
+    }
+
+    @Test
+    public void update_throwsExceptionWhenBookWasNotFound() {
+        Mockito.when(mockCakeRepository.findById(id)).thenReturn(Optional.empty());
+        CakeNotFoundException exception = assertThrows(CakeNotFoundException.class, () -> cakeService.update(input, id));
+        assertEquals("A cake with id: " + id + " was not found.", exception.getMessage());
+    }
+
+    @Test
+    public void patch_throwsExceptionWhenBookWasNotFound() {
+        Mockito.when(mockCakeRepository.findById(id)).thenReturn(Optional.empty());
+        CakeNotFoundException exception = assertThrows(CakeNotFoundException.class, () -> cakeService.patch(input, id));
+        assertEquals("A cake with id: " + id + " was not found.", exception.getMessage());
+
+    }
 }
